02daf347f76f85c25a85c8008eaa3c664f1975c0
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 17:45:27 -0600

    Create 03 - Capitulo 3 - Strings.py
    PODEMOS IMPRIMIR CON COMILLAS SIMPLES Y DOBLES LO QUE QUERAMOS QUE APAREZCA EN LA PANTALLA A ESTO SE LE LLAMA Strings

---
+++ 03 - Capitulo 3 - Strings.py
@@ -0,0 +1,6 @@
+mensaje = "esto es un strings 'hola'"
+print(mensaje)
+
+texto = 'esto es un strings "buenas noches"'
+print(texto)
+# PODEMOS IMPRIMIR CON COMILLAS SIMPLES Y DOBLES LO QUE QUERAMOS QUE APAREZCA EN LA PANTALLA
