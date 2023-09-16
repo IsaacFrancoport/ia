df78226e7db008ddcd987d5316659f297cb92e04
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 17:52:47 -0600

    05 - Capitulo 5 - Los métodos upper, lower y title()
    El title() se utiliza para poner las primeras iniciales en mayúscula, upper ()se utiliza para poner todas las letras en mayúscula, lower se utiliza para poner las letras en mayúscula

---
+++ 05 - Capitulo 5 Los métodos upper(), lower() y title().py
@@ -0,0 +1,11 @@
+nombre = "programación fácil"
+nombre = nombre.title()  # el title() se utiliza para poner las primeras iniciales en mayuscula, upper ()se utiliza para poner todas las letras en mayuscula
+print(nombre)            # lower se utilza para poner las letras en miuscula 
+
+nombre = "programación fácil"
+nombre = nombre.lower()
+print(nombre)
+
+nombre = "programación fácil"
+nombre = nombre.upper()
+print(nombre)
