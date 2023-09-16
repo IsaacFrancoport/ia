52a582e72807548610e78d9b4d59865858148501
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 17:49:27 -0600

    Create 04 - Capitulo 4 - Cómo concatenar.py
      Se utiliza el mas para poder juntar palabras dependiendo el texto que pongas , Nos sirve para juntar oraciones

---
+++ 04 - Capitulo 4 - Cómo concatenar.py
@@ -0,0 +1,7 @@
+primerapalabra = 'Programación'
+
+segundapalabra = 'cencillo'
+
+nombrecompleto = primerapalabra + segundapalabra    # se utiliza el mas para poder juntar palabras dependiendo el texto que pongas     
+
+print(nombrecompleto)
