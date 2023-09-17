ce7e235986e3bd5c8fae939c919782c79cb6def8
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:06:20 -0600

    06 - Capitulo 6 - saltos de línea y tabulaciones
    # el salto de línea lo podemos ver de tres manera, con los print podemos hacer saltos de línea pero es lo básico, también esta \n ese es otro salto de línea y también esta \n\t ese también te da un espacio de línea y también de espacio

---
+++ 06 - Capítulo 6 - Saltos de línea y tabulaciones.py
@@ -0,0 +1,12 @@
+# NOMBRE DEL ALUMNO: ISAAC EMMANUEL FRANCO GARCIA
+# REGISTRO: 21110355
+
+print("Python")
+print("JavaScript")
+print("Java")
+print("PHP")
+print("TypeScript")
+print("Python\nJavaScript\nJava\nPHP\nTypeScript")
+
+cursos = "Lista de cursos de programación:\n\tPython\n\tJavaScript\n\tJava\n\tPHP\n\tTypeScript"
+print(cursos) # el salto de linea lo podemos ver de tres manera, con los print podemos hacer saltos de linea pero es lo basico, tambien esta \n ese es otro salto denlinea y tambuien esta \n\t ese tambien te da un espacio de linea y tambien de espacio 
