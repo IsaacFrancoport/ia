4aee50c1ce2b0b822eb50969c9092005c04fcaf8
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:50:22 -0600

    31 - Capítulo 31 - Qué son los diccionarios de Python
    EN ESTE CODIGO PODEMOS VER COMO SE PUEDEN HACER UN MENU, PODEMOS HACER QUE SE NOS MUETRE TANTO EL MENU COMPLETO COMO POR PARTES DESDE LAS BEVIDAD COMO LAS COMIDAS

---
+++ 31 - Capítulo 31 - Qué son los diccionarios de Python.py
@@ -0,0 +1,17 @@
+print("menu de comida y de bebida")
+
+menu1 = {
+    "bevidas sin alcohol": "refresco",
+    "comida": "comida a la mexicana", 
+    "precio": "50,80"
+
+}
+
+menu2 = {
+    "bebidas con alcohol": "tequila",
+    "comida": "alitas",
+    "precio": "70,80",
+}
+muestramenu = dict(menu1)
+print(muestramenu)
+# EN ESTE CODIGO PODEMOS VER COMO SE PUEDEN HACER UN MENU, PODEMOS HACER QUE SE NOS MUETRE TANTO EL MENU COMPLETO COMO POR PARTES DESDE LAS BEVIDAD COMO LAS COMIDAS
