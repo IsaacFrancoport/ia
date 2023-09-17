e69a4c7efa03c5207b759152ac8a551ea863394c
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:52:55 -0600

    33 - Capítulo 33 - Métodos con diccionarios.
    CON ESTE CODIGO NOS PODEMOS DAR CUANTA DE CUANTOS ELEMENTOS TIENE UN MENU O CUANTOS MENUS TIENE DENTRO DEL CODIGO

---
+++ 33 - Capítulo 33 - Métodos con diccionarios.py
@@ -0,0 +1,19 @@
+
+print(" bienvenido al menu de comida y de bebida")
+
+menu1 = {
+    "bevidas sin alcohol": "refresco",
+    "comida": "comida a la mexicana", 
+    "precio": "50,80"
+
+}
+
+menu2 = {
+	"bebidas con alcohol": "tequila",
+    "comida": "alitas",
+    "precio": "70,80",
+	}
+print(len(menu1)) #con este print nos sirve para que muentre cuantos ELEMENTOS TIENE DENTRO DEL MENU
+
+del menu1['comida']
+print(menu1) # CON ESTA PARTE DEL CODOGO NO ELIMINA UN ELEMENTO POR EJEMPLO ESTOY QUITANDO COMIDA Y SOLO QUEDA REFRESCO Y PRECIO
