2efc330d1ab08d149b76077b4b081920156fc238
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:23:01 -0600

    13 - Capitulo 13 Eliminar elementos con remove
     con remove podemos eliminar cualquier cosa que este dentro de los corchetes y no importa en que lugar este con poner remove se elimina automáticamente

---
+++ 13 - Capitulo 13 Eliminar elementos con remove.py
@@ -0,0 +1,4 @@
+colores = ['naraja', 'azul', 'negro', 'amarillo']
+colores.remove('azul')
+print(colores)
+# con remove podemos eliminar cualquier cosa que este dentro de los corchetes y no importa en que lugar este con poner remove se elimina automaticamente 
