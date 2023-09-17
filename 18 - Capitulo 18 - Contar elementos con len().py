a65205419cd38ecbe4ff45c4137308733a2e6a44
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:32:01 -0600

    18 - Capitulo 18 - Contar elementos con len()
     CON ESTE CODIGO PODEMOS SABER CUANTOS NOMBRES U/O OBJETOS TENEMOS DENTRO DE LA VARIABLE Y ESTA BARIABLE LEN NOS PERMITE SABER CUANTOS NOMBRES AHI SIN NECECIDAD DE ESTRAR CONTANDO UNO POR UNO

---
+++ 18 - Capitulo 18 - Contar elementos con len().py
@@ -0,0 +1,9 @@
+texto = ['Perro', 'Gato', 'Elefante', 'León', 'Tigre', 'Oso', 'Jirafa', 'Cebra', 'Lobo', 'Koala', 'Pingüino', 'Delfín',  'Ballena',  'Gorila', 
+'Chimpancé',  'Rana',  'Serpiente', 'Águila', 'Búho', 'Mariposa', 'Tortuga', 'Cocodrilo', 'Pez', 'Caballo', 'Vaca', 'Canguro', 'Pato', 'Paloma', 'Puma', 'Zorro',
+'Murciélago', 'Colibrí', 'Rinoceronte', 'Hiena', 'Nutria', 'Orca', 'Camello', 'Puercoespín', 'Armadillo', 'Ciervo']
+
+print(len(texto))
+# CON ESTE CODIGO PODEMOS SABER CUANTOS NOMBRES U/O OBJETOS TENEMOS DENTRO DE LA BARIABLE Y ESTA BARIABLE LEN NOS PERMITE SABER CUANTOS NOMBRES AHI SIN NECECIDAD DE ESTRAR CONTANDO UNO POR UNO
+
+print(texto[39])
+#CON AYUDA DE STE PRINT POIDEMOS AGRAGARLE EL ULTIMO NUMRO DIGAMOS EL 39 Y NO MUETRA QUE ES EL CIERVO Y ESTO INDICA QUE ETA BIEN 
