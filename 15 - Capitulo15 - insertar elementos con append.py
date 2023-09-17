73901c7ba424b5b90af42cfce1b0a36de47f2c3c
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:27:09 -0600

    15 - Capitulo15 - insertar elementos con append
    CON ESTE CODIGO PUEDO AGREGAR NOMBRES CON APPEND Y ASI PUEDO AGRAGAR MUCHOS NOMBRES

---
+++ 15 - Capitulo15 - insertar elementos con append.py
@@ -0,0 +1,4 @@
+Animales = ['leon', 'jirafa', 'hipopotamo', 'iguana']
+Animales.append('raton')
+print(Animales)
+# CON ESTE CODIGO PUEDO AGREGAR NOMBRES CON APPEND Y ASI PUEDO AGRAGAR MUCHOS NOMBRES 
