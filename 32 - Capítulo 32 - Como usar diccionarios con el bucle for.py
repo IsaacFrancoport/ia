f75eab77e398f236965947fd94b390ffe7ed020e
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:51:19 -0600

    32 - Capítulo 32 - Como usar diccionarios con el bucle for
    EN ESTE CODIGO PODEMOS VER COMO SE PUEDEN HACER UN MENU, PODEMOS HACER QUE SE NOS MUETRE TANTO EL MENU COMPLETO COMO POR PARTES DESDE LAS BEVIDAD COMO LAS COMIDAS

---
+++ 32 - Capítulo 32 - Como usar diccionarios con el bucle for.py
@@ -0,0 +1,19 @@
+print(" bienvenido al menu de comida y de bebida")
+
+menu1 = {
+    "bevidas sin alcohol": "refresco",
+    "comida": "comida a la mexicana", 
+    "precio": "50,80"
+
+}
+
+menu2 = {
+	"bebidas con alcohol": "tequila",
+    "comida": "alitas",
+    "precio": "70,80",
+	}
+
+
+for x in menu2:
+	print(x)
+	#en este menu podemos ver que solamente se imprime lo de bevidas, comida y precio 
