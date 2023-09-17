d2a3bc989ff2a8d1ac2d0d68b5d79415a09877a0
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:45:08 -0600

    25 - Capítulo 25 - Múltiples condiciones IF
    EN ESTE CODIGO SE HACE UN MENU DE OPCIONES CON PURO IF LO CUAL CUANDO SE PONGA LA OPCION DE UN IF SALGA LO QUE TENGA ESCRITO DE LO CONTRARIO NO SALDRA NADA

---
+++ 25 - Capítulo 25 - Múltiples condiciones IF.py
@@ -0,0 +1,14 @@
+entrada = int(input('Introduce un número del 1 al 4:\n'))
+
+if entrada == 1:
+    print('Has seleccionado la opción número 1.')
+if entrada == 2:
+    print('Has seleccionado la opción número 2.')
+if entrada == 3:
+    print('Has seleccionado la opción número 3.')
+if entrada == 4:
+    print('Has seleccionado la opción número 4.')
+
+else:
+    print("introduce un numero del 1 al 4:")
+#EN ESTE CODIGO SE HACE UN MENU DE OPCIONES CON PURO IF LO CUAL CUANDO SE PONGA LA OPCION DE UN IF SALGA LO QUE TENGA ESCRITO DE LO CONTRARIO NO SALDRA NADA 
