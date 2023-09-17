91b75b22ab5b1687a9d9d870912088b51a8395e5
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:19:30 -0600

    10 - Capitulo 10 - Cómo crear listas y utilizarlas
    TE ENMOS TRES TIPOS DE VARIABLES LO CUAL CADA UNA HACE UNA FUNCION MUY DIFERENTE POR EJEMPLO:
    *CON ESTA VARIABLE PUEDE IMPRIMIR TODO LO QUE ESTA ESCRITO DENTRO DE LOS PARENTESIS 
    *CON EESTA VARIABLE SOLAMENTE ESTA LLAMNDO A IPHONE LO CUAL ES LO QUE SE VA IMPRIMIR EN LA PANTALLA
    *EN ESTA CARIABLE SOLAMENTE ESTA LLAMNADO A XIAOMI LO CUAL EL PRIMER DIJITO LO CUNETA DESDE EL 0, 1, 2, 3, ETC Y ASI SUCESIBAMNETNTE

---
+++ 10 - Capitulo 10 -  Cómo crear listas y utilizarlas.py
@@ -0,0 +1,9 @@
+smartphones = ['Xiaomi', 'Iphone', 'Huawei', 'Samsung']
+print(smartphones)
+# CON ESTA VARIABLE PUEDE IMPRIMIR TODO LO QUE ESTA ESCRITO DENTRO DE LOS PARENTESIS 
+smartphones = ['Xiaomi', 'Iphone', 'Huawei', 'Samsung']
+print(smartphones[2])
+# CON EESTA VARIABLE SOLAMENTE ESTA LLAMNDO A IPHONE LO CUAL ES LO QUE SE VA IMPRIMIR EN LA PANTALLA
+smartphones = ['Xiaomi', 'Iphone', 'Huawei', 'Samsung']
+print(smartphones[0])
+# EN ESTA CARIABLE SOLAMENTE ESTA LLAMNADO A XIAOMI LO CUAL EL PRIMER DIJITO LO CUNETA DESDE EL 0, 1, 2, 3, ETC Y ASI SUCESIBAMNETNTE 
