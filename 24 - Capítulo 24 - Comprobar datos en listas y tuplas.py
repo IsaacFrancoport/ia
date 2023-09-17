711af97de290e938dc8c9135f940fa03116c90d2
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:44:25 -0600

    24 - Capítulo 24 - Comprobar datos en listas y tuplas
    A QUI TE NEMOS UNA LISTA DE PENDE SI EL NOMBRE DEL ANIMAL ESTA TA VA APARECER UN PRINT CON SU DEFINICION

---
+++ 24 - Capítulo 24 - Comprobar datos en listas y tuplas.py
@@ -0,0 +1,8 @@
+entrada = input('Introduce el nombre de un animal:\n')
+navegadores = ['raton', 'leon', 'jirafa', 'perro']
+if entrada in navegadores:
+	print('El animal que buscas está en la lista.')
+else:
+	print('El amimal que buscas no está en la lista.')
+	
+	
