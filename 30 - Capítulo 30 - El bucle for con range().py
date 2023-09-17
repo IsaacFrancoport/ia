65291989c3e2b0a777d1cc8685c05941ca2ce78e
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:49:26 -0600

    30 - Capítulo 30 - El bucle for con range
     ESTE CODIGO NOS AYUDA APODER MULTIPLICAR LOS NUMERO SIN NECESIDAD DE ESCRIBIR TANTO CODIGO

---
+++ 30 - Capítulo 30 - El bucle for con range().py
@@ -0,0 +1,6 @@
+print("numeros de la lista:\n")
+numeros = [2, 5, 8, 16, 32, 64, 128]
+
+for x in range( len(numeros)):
+ print("numero de operacion ->", x, "multiplicacion ->", numeros[x], "resultado ->", numeros[x] * numeros[x])
+# ESTE CODIGO NOS AYUDA APODER MULTIPLICAR LOS NUMERO SIN NECESIDAD DE ESCRIBIR TANTO CODIGO
