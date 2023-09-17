ae6015280caef0ec82b616a9e8a827090a5cdb30
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:21:59 -0600

    12 - Capitulo 12 - Eliminar elementos con del()
    A QUI LO QUE ESTAMOS VIENDO ES COMO PODER ELIMINAR ALGO QUE ESTA ESCRITO EN UNA VARIABLE COMO POR EJEMPLO PODEMOS ELIMINAR "Leon" PONIENDO SU VALOR QUE SERIA EL MENOS -10 Y DE ESTA MANERA ELIMINAR ESE NOMBRE

---
+++ 12 - Capitulo 12 - Eliminar elementos con del().py
@@ -0,0 +1,6 @@
+animales = ['leon', 'jirafa', 'pajaro', 'rinoceronte', 'cocodrilo', 'tiburon', 'leopardo', 'chimpance', 'gorila', 'venado']
+del animales[-10]
+print(animales)
+
+
+# A QUI LO QUE ESTAMOS VIENDO ES COMO PODER ELIMINAR ALGO QUE ESTA ESCRITO EN UNA VARIABLE COMO POR EJEMPLO PODEMOS ELIMINAR "Leon" PONIENDO SU VALOR QUE SERIA EL MENOS -10 Y DE ESTA MANERA ELIMINAR ESE NOMBRE 
