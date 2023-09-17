da9faf7538fcf38972a68c071b23189e72e46b5d
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:33:28 -0600

    19 - Capítulo 19 - Crear y manejar tuplas
    COMO PODEMOS VER PARA LA LISTA SE PONE CORCHETES Y PARA LA TUPLA SE PONE PARENTESIS CADA UNA SE UTILIZA DE DIFERENTSES MANERAS

---
+++ 19 - Capítulo 19 - Crear y manejar tuplas.py
@@ -0,0 +1,10 @@
+
+lista = ['rojo', 'azul', 'verde', 'amarillo']
+tupla = ('rojo', 'azul', 'verde', 'amarillo')
+# COMO PODEMOS VER PARA LA LISTA S EPONE CORCHETES Y PARA LA TUPLA SE PONE PARENTESIS CADA USA SE UTILIZA DE DIFERENTSES MANERAS 
+tupla = (10, 15, 20, 'El resultado de la operación es:')
+print(tupla[3], tupla[2] + tupla[0] * tupla[1] / tupla[0])
+# CON ESTA VARIABLE TUPLA NOS SIRVE PARA YA NO MIDIFICAR NADA CUANDO ESTE EN UNA APLICACION SE PUDE MODIFICAR DESDE EL IDE 
+
+tupla = ('leon', 'jirafa', 'perro', 'raton', 'elefante', 'MUESTRAME DOS NOMBRES')
+print(tupla[5], tupla[1] + tupla[3])
