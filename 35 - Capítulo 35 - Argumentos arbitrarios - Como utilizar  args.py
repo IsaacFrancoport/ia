82384087bfd8cc0159b63315877b618128d60fe0
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:54:54 -0600

    35 - Capítulo 35 - Argumentos arbitrarios - Como utilizar args
    ESTE CODIGO NOS MUETRA  CUAL ES EL ULTIMO TRBAJADOR Y EL PRIMER TRABAJADOR

---
+++ 35 - Capítulo 35 - Argumentos arbitrarios - Como utilizar  args.py
@@ -0,0 +1,6 @@
+def trabajadores(*argumentos):
+    print(" el ultimo trabajador es " + argumentos[3] + " el primer trabajador es " + argumentos[0] + ".")
+
+trabajadores("felipe", "ramon", "beto", "luis")
+# ESTE CODIGO NOS MUETRA  CUAL ES EL ULTIMO TRBAJADOR Y EL PRIMER TRABAJADOR
+    
