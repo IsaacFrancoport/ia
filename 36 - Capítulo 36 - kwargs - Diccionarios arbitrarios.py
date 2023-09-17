237bbb176211bf58ddc3d30013e44262ee7b24fb
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:55:29 -0600

    36 - Capítulo 36 - kwargs - Diccionarios arbitrarios
    EN ESTE CODIGO NOS MUESTRA EL COLOR QUE SE ESTA HACIENDO MENCION POR MEDIO DE LA VARIABLE KWARGS COLOR1 EL CUAL EL RESULTADO ES NARANJA

---
+++ 36 - Capítulo 36 - kwargs - Diccionarios arbitrarios.py
@@ -0,0 +1,5 @@
+def colores (**kwargs):
+	print("Este es el color " + kwargs['color1'] + '.')
+
+colores(color1='naraja', color2='verde')
+# EN ESTE CODIGO NOS MUESTRA EL COLOR QUE SE ESTA HACIENDO MENCION POR MEDIO DE LA VARIABLE KWARGS COLOR1 EL CUAL EL RESULTADO ES NARANJA
