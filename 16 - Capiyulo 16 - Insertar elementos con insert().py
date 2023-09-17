300f1cd57c64b55735b33acda238c924624efaa8
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:27:56 -0600

    16 - Capiyulo 16 - Insertar elementos con insert()
    EN ESTE CODIGO PODEMOS AGREGAR CUALQUIER ELEMENTO O NOMBRE CON INSERT QUE NOS SIRVE PARA AGREGAR, ES FUNDAMETAL PARA AGRGAR COSAS EN EL CODIGO

---
+++ 16 - Capiyulo 16 - Insertar elementos con insert().py
@@ -0,0 +1,4 @@
+animales = ['raton', 'jirafa', 'iguana', 'anaconda']
+animales.insert(0,'leon')
+print(animales)
+# EN ESTE CODIGO PODEMOS AGREGAR CUALQUIER ELEMENTO O NOMBRE CON INSERT QUE NOS SIRVE PARA AGREGAR, ES FUNDAMETAL PARA AGRGAR COSAS EN EL CODIGO
