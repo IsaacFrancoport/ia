f2d4eefce98de040fb73160ffb64deffdeaa78b4
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:25:56 -0600

    14 - Capitulo14 - Eliminar elementos con pop
    En este codigo nos enseña a como eliminar y guardar a la vez un nombre o tambien algun onjeto.

---
+++ 14 - Capitulo14 - Eliminar elementos con pop.py
@@ -0,0 +1,5 @@
+
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+almacena_valor = colores.pop(2)
+print('El color eliminado y almacenado es:',  almacena_valor)
+# En este codigo nos enseña a como eliminar y guardar a la vez un nombre o tambien algun onjeto.
