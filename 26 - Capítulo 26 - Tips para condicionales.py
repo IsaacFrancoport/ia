7a5dbcc940583028afd7390365f781ace0844e1f
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:45:52 -0600

    26 - Capítulo 26 - Tips para condicionales.
    EN ESTE CODIGO PODEOS VER COMO FUNCIONA UN IF Y UN ELSE CON DIFERENETES FUNCIONAMIENTOS

---
+++ 26 - Capítulo 26 - Tips para condicionales.py
@@ -0,0 +1,13 @@
+x = 200
+y = 300
+z = 1000
+
+if x < y:
+    print(" x es menor que y", "si se cumple la funcion")
+
+else:
+    print("y es mayor que x", "no se cumple la funcion")
+# EN ESTE CODIGO PODEOS VER COMO FUNCIONA UN IF Y UN ELSE CON DIFERENETES FUNCIONAMIENTOS 
+
+if z > y:
+    print("z es mayor que y", "si cimple la funcion")
