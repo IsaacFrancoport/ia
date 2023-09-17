407fad7eb5ecf6e1bc9865f0c85f04b6a0903c00
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:09:50 -0600

    07 - capitulo7 matemáticas básicas
    A qui esta una operación básica de matemáticas, lo cual es importante saber esto ya que puedes hacer una calculadora con esto o hacer operaciones mas complejas

---
+++ 07 - capitulo7 matematicas basicas.py
@@ -0,0 +1,12 @@
+suma = 10 + 5 + 7
+resta = 10 - 5 - 7
+multiplicacion = 10 * 5 * 7
+division = 10 / 5 / 7
+print("Suma: " , suma ,  "\nResta: " , resta , "\nMultiplicación: " , multiplicacion , "\nDivisión: " , division)
+# vemos que aqui esta siguendo un orden para cada variable en suma  solmente lo esta sumando y asi sucesiba mente con cada variable
+operacion = 10 - 5 * 7 / 3 + 5
+print("Resultado: " , operacion)
+# vemos otra manera de como podemos hacer operaciones matematicas, primoro resta el 10 menos 5 y despues lo multiplica por el 7 depues lo divide entre 3 y lo suma por el numero 5 
+operacion = (10 - 5) * 7 / 3 + 5
+print("Resultado: " , operacion)
+# A qui esta haciendo primero la resta de los parentesis el resultado lo multiplica ccon el 7 y despues lo divide entre 3 y lo suma con el 5 este nos puede ayudar para calcular ya en manera mas profesional
