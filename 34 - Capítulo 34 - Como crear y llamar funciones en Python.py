d72d8416d790a0ae4b764b97e9a7ad757a254c9c
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-09-16 18:54:09 -0600

    34 - Capítulo 34 - Como crear y llamar funciones en Python.
    CON ESTE CODIGO PODENMOS AGREGAR NOMBRES U/O OBJETOS Y CREAR LISTAS EXTENSAS PANOMBRES

---
+++ 34 - Capítulo 34 - Como crear y llamar funciones en Python.py
@@ -0,0 +1,11 @@
+print("la familia garcia\n")
+
+def familia(nombre,parentesco):
+    print(nombre + " garcia " + parentesco)
+familia("bety","tia") 
+familia("ramon", "tio") 
+familia("cecy", "tia") 
+familia("pricila", "prima") 
+familia("cynthya", "prima") 
+familia("maria elena", "mama")    
+# CON ESTE CODIGO PODENMOS AGREGAR NOMBRES U/O OBJETOS Y CREAR LISTAS EXTENSAS PANOMBRES 
